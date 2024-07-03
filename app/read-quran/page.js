c8c281b0b56634a3242edacf62837cc76b8dcc40
muhammadrayhanr <mrayhanr4869@gmail.com>
2024-07-03 15:56:02 +0700

    quran online

---
+++ app/read-quran/page.js
@@ -0,0 +1,71 @@
+'use client';
+import axios from 'axios';
+import { useRouter, useSearchParams } from 'next/navigation';
+import React, { useEffect, useState } from 'react';
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
+import { ReloadIcon } from '@radix-ui/react-icons';
+
+const Index = () => {
+  const searchParams = useSearchParams();
+  const numberSurah = searchParams.get('surah');
+  const [data, setData] = useState(null); // Ubah menjadi null untuk menunjukkan bahwa data belum dimuat
+  const router = useRouter();
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_KEY}/surah/${numberSurah}.json`)
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, [numberSurah]); // Tambahkan numberSurah sebagai dependency
+
+  if (!data) {
+    return (
+      <div className="flex min-h-screen flex-col items-center bg-[#fceddc]">
+        <p className='m-auto'><ReloadIcon className="animate-spin w-6 h-6" /></p>
+      </div>
+    ); // Menampilkan loading jika data masih dimuat
+  }
+
+  const handlePush = () => {
+    router.push('/');
+  };
+
+  return (
+    <div className="relative bg-[#fceddc]">
+      <header className="absolute top-0 p-3 bg-[#145750] w-full text-center text-white">
+        <span
+          className="font-bold cursor-pointer"
+          onClick={() => handlePush()}>
+          Al-Qur`an
+        </span>
+      </header>
+      <main className="flex min-h-screen flex-col items-center px-0 pt-12 md:p-12">
+        <h1 className="mt-8 font-semibold text-lg">{data.name}</h1>
+        <p className="text-xl">{data.name_translations.ar}</p>
+        <p className="text-sm">{data.name_translations.id}</p>
+
+        <Table className="my-10">
+          <TableBody>
+            {data.verses.map((item) => (
+              <TableRow key={item.number}>
+                <TableCell className="w-[30px]">
+                  <div className="flex justify-between gap-2">
+                    <span className="text-md flex items-center">{item.number}.</span>
+                    <p className="text-3xl text-end">{item.text}</p>
+                  </div>
+                  <p className="py-3">{item.translation_id}</p>
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </main>
+    </div>
+  );
+};
+
+export default Index;
---
+++ components/molecules/TableData.jsx
@@ -0,0 +1,104 @@
+import { Table, TableBody, TableCell, TableRow } from '@/components/ui/table';
+import { MagnifyingGlassIcon, PauseIcon } from '@radix-ui/react-icons';
+import axios from 'axios';
+import { PlayIcon } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import { useEffect, useState, useRef } from 'react';
+
+export function TableData() {
+  const [playingIndex, setPlayingIndex] = useState(null);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const audioRef = useRef(new Audio());
+  const router = useRouter();
+
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${process.env.NEXT_PUBLIC_BASE_KEY}/quran.json`)
+      .then(function (response) {
+        setData(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
+
+  // Function to handle both play and pause operations
+  const playPause = (index, url) => {
+    if (playingIndex === index) {
+      if (!audioRef.current.paused) {
+        audioRef.current.pause();
+        setIsPlaying(false);
+      } else {
+        audioRef.current.play();
+        setIsPlaying(true);
+      }
+    } else {
+      if (playingIndex !== null) {
+        audioRef.current.pause();
+      }
+      audioRef.current.src = url;
+      audioRef.current.play();
+      setPlayingIndex(index);
+      setIsPlaying(true);
+    }
+  };
+
+  // Event listeners to update state when audio is played or paused
+  useEffect(() => {
+    const handlePlay = () => setIsPlaying(true);
+    const handlePause = () => setIsPlaying(false);
+
+    const audio = audioRef.current;
+    audio.addEventListener('play', handlePlay);
+    audio.addEventListener('pause', handlePause);
+
+    return () => {
+      audio.removeEventListener('play', handlePlay);
+      audio.removeEventListener('pause', handlePause);
+    };
+  }, []);
+
+  // Cleanup function to pause the audio when the component is unmounted
+  useEffect(() => {
+    return () => {
+      audioRef.current.pause();
+    };
+  }, []);
+
+  const onClickHandler = (id) => {
+    router.push(`/read-quran?surah=${id}`)
+  }
+
+  return (
+    <Table className="my-10">
+      <TableBody>
+        {data.map((item, index) => (
+          <TableRow key={item.number_of_surah}>
+            <TableCell className="w-[30px] text-center text-xs">{item.number_of_surah}</TableCell>
+            <TableCell className="font-medium text-sm">
+              <div className="flex justify-between">
+                <div>
+                  <p>{item.name}</p>
+                  <p className="text-xs text-slate-700">
+                    {item.type} | {item.number_of_ayah} Ayat
+                  </p>
+                </div>
+              </div>
+            </TableCell>
+            <TableCell className="text-end font-semibold">{item.name_translations.ar}</TableCell>
+            <TableCell className="text-center">
+              <button onClick={() => playPause(index, item.recitation)}>{playingIndex === index && isPlaying ? <PauseIcon className="w-5 h-5" /> : <PlayIcon className="w-5 h-5" />}</button>
+            </TableCell>
+            <TableCell>
+              <button onClick={() => onClickHandler(item.number_of_surah)}>
+                <MagnifyingGlassIcon className="w-6 h-6" />
+              </button>
+            </TableCell>
+          </TableRow>
+        ))}
+      </TableBody>
+    </Table>
+  );
+}
